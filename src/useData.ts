2d2211ef5b0b77dc60ca4149d908b26868c158af
swon95 <shimtkddnjs13@gmail.com>
2023-08-24 01:28:53 +0900

    refactor: types declaration

--- src/useData.ts
+++ src/useData.ts
@@ -2,22 +2,9 @@ import { useCallback, useState } from 'react'
 import { TypeListItem } from './TypeListItem'
 import axios from 'axios'
 
-// 동일한 config 값 들을 가지고있는 axiosInstance
-const axiosInstance = axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3/', // 주로 서버 URL == host
-})
-
-const API_KEY = 'AIzaSyApZmz9J1Z3ekIfhKKfByc_qvyhomHApMI'
-
-export const useData = () => {
-    const [ data, setData ] = useState<TypeListItem[]>([])
-    
-    const loadData = useCallback(async() => {
-        
-        try {
-            // baseURL 로 get 요청 => '/video' 엔드포인트로 요청, 파라미터로는 호출할 때 사용되는 config(매개변수) 값
-            const videoResults = await axiosInstance.get<{
-                kind: 'youtube#videoListResponse';
+// type 재 선언
+type TypeVideoListResponse = {
+  kind: 'youtube#videoListResponse';
                 etag: string;
                 nextPageToken: string;
                 prevPageToken: string;
@@ -82,7 +69,24 @@ export const useData = () => {
                       commentCount: number;
                     }
                 }[]
-            }>('/videos', {
+            }
+// 동일한 config 값 들을 가지고있는 axiosInstance
+const axiosInstance = axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3/', // 주로 서버 URL == host
+})
+
+const API_KEY = 'AIzaSyApZmz9J1Z3ekIfhKKfByc_qvyhomHApMI'
+
+export const useData = () => {
+    const [ data, setData ] = useState<TypeListItem[]>([])
+    
+    const loadData = useCallback(async() => {
+        
+        try {
+            // baseURL 로 get 요청 => '/video' 엔드포인트로 요청, 파라미터로는 호출할 때 사용되는 config(매개변수) 값
+            const videoResults = await axiosInstance.get<TypeVideoListResponse>(
+              '/videos', 
+              {
                 params: {
                     key: API_KEY,
                     part: 'snippet, contentDetails, statistics',
@@ -90,7 +94,8 @@ export const useData = () => {
                     // 국가코드
                     regionCode: 'KR',
                 },
-            })
+              }
+            )
             
             const videoData = videoResults.data
             setData(videoData.items.map(( item ) => ({
